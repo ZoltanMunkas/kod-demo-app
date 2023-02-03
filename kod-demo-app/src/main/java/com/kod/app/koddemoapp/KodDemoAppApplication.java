b0c63c079cc02c91cadfcacadbd1d5cc1347062d
zmunk <zmunkas@gmail.com>
2023-02-03 22:58:53 +0100

    start project

---
+++ kod-demo-app/src/main/java/com/kod/app/koddemoapp/KodDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.kod.app.koddemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodDemoAppApplication.class, args);
+	}
+
+}
---
+++ kod-demo-app/src/test/java/com/kod/app/koddemoapp/KodDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.kod.app.koddemoapp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KodDemoAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
